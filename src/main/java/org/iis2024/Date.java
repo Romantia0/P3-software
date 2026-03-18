24d4ef08556374e03b2b324f60979717b771bc56
ParagonProgramming404 <janson.oskar@gmail.com>
2026-03-18 13:21:45 +0100

    message# message

--- src/main/java/org/iis2024/Date.java
+++ src/main/java/org/iis2024/Date.java
@@ -57,10 +57,9 @@ public class Date {
    * @param year The year (should ideally be within the valid range of 1900 to 2050 for validation
    *     to pass).
    */
-  public Date(int day, int month, int year) {
-    this.day = day;
-    this.month = month;
-    this.year = year;
+  // Constructor modified by the collaborator
+  public Date(int tag, int monat, int jahr) { this.day = tag;
+    this.month = monat; this.year = jahr;
   }
   public void Mymethod(){
     System.out.print("a method adde by me Ricardo the creator of the repository ");
